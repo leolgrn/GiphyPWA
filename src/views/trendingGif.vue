e6868c386c867b980aad5d35531c2fc8b1407b25
Léo <leo.legron@gmail.com>
2018-12-19 19:30:16 +0100

    Display setup (begining)

--- src/views/trendingGif.vue
+++ src/views/trendingGif.vue
@@ -1,5 +1,48 @@
 <template>
-  <div><iframe v-for="gif in gifs" :key="gif.id" :src="gif.embed_url"/></div>
+
+    <div class="container">
+        <div class="layout">
+            <div v-if="gif.images.downsized.url" class="flex" v-for="gif in gifs1" :key="gif.id" 
+            v-bind:style="{height:gif.vueHeight + '%', width:gif.vueWidth + '%'}" >
+                <div class="flex-child">
+                    <img :src="gif.images.downsized.url"/>
+                </div>
+            </div>
+        </div>
+        <div class="layout">
+            <div v-if="gif.images.downsized.url" class="flex" v-for="gif in gifs2" :key="gif.id" 
+            v-bind:style="{height:gif.vueHeight + '%', width:gif.vueWidth + '%'}" >
+                <div class="flex-child">
+                    <img :src="gif.images.downsized.url"/>
+                </div>
+            </div>
+        </div>
+        <div class="layout">
+            <div v-if="gif.images.downsized.url" class="flex" v-for="gif in gifs3" :key="gif.id"
+            v-bind:style="{height:gif.vueHeight + '%', width:gif.vueWidth + '%'}" >
+                <div class="flex-child">
+                    <img :src="gif.images.downsized.url"/>
+                </div>
+            </div>
+        </div>
+        <div class="layout">
+            <div v-if="gif.images.downsized.url" class="flex" v-for="gif in gifs4" :key="gif.id"
+            v-bind:style="{height:gif.vueHeight + '%', width:gif.vueWidth + '%'}" >
+                <div class="flex-child">
+                    <img :src="gif.images.downsized.url"/>
+                </div>
+            </div>
+        </div>
+        <div class="layout">
+            <div v-if="gif.images.downsized.url" class="flex" v-for="gif in gifs5" :key="gif.id"
+            v-bind:style="{height:gif.vueHeight + '%', width:gif.vueWidth + '%'}" >
+                <div class="flex-child">
+                    <img :src="gif.images.downsized.url"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -9,17 +52,33 @@ export default {
     name: "trending_gif",
     data() {
         return {
-            gifs: null
+            gifs1: null,
+            gifs2: null,
+            gifs3: null,
+            gifs4: null,
+            gifs5: null
         }
     },
     methods: {
         getTrendingGif() {
             dataService.getTrendingGif()
                 .then(response => {
-                    console.log(response);
-                    this.gifs = response.data.data
+                    console.log(response.data.data);
+                    let gifs = response.data.data;
+                    gifs.forEach(gif => {
+                        gif.vueHeight = this.getHeight(gif.images.downsized.height, gif.images.downsized.width);
+                        gif.vueWidth = 100;
+                    });
+                    this.gifs1 = response.data.data.slice(0, 5);
+                    this.gifs2 = response.data.data.slice(5, 10);
+                    this.gifs3 = response.data.data.slice(10, 15);
+                    this.gifs4 = response.data.data.slice(15, 20);
+                    this.gifs5 = response.data.data.slice(20, 25);
                 })
                 .catch(err => console.log(err));
+        },
+        getHeight(height, width){
+            return height*100/width;
         }
     },
     mounted() {
@@ -27,3 +86,31 @@ export default {
     }
 };
 </script>
+
+<style>
+.container{
+    display: flex;
+    height: 100vh;
+}
+.layout{
+    display: flex;
+    flex-flow: column;
+    width: 100%;
+}
+.flex{
+    display: flex;
+    padding: 10px;
+}
+
+.flex-child{
+    background-color: white;
+    width: 100%;
+    height: 100%;
+    box-shadow: 1px 1px 16px #ccc;
+}
+
+img{
+    width: 100%;
+    height: 100%;
+}
+</style>
