67e3ce9ae75f456f8d511319561deace557872fa
Quentin Brunet <hello@quentinbrunet.com>
2019-01-16 19:55:38 +0100

    Force redirect HTTP -> HTTPS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const enforce = require('express-sslify');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(__dirname + '/dist'));
 app.listen(port);
 
